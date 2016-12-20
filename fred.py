5167d0635230933194326e0a52a7b804c7b74e42
Frederic Lafrance <lafrancef@gmail.com>
2016-12-20 19:27:56 +0100

    Added multilabel approach

---
+++ fred.py
@@ -0,0 +1,79 @@
+import numpy as np
+import nilearn.image
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+
+from multilabel import *
+
+
+path_train = '../data/set_train/mri/'#4mm/'
+path_test = '../data/set_test/mri/'#4mm/'
+
+train_size = 278
+test_size = 138
+
+cut = {}
+cut['cut_x'] = (16, 160) # original 18 158
+cut['cut_y'] = (19, 190) # original 19 189
+cut['cut_z'] = (11, 155) # original 13 153
+
+
+x = []
+t = []
+
+for i in range(0, train_size):
+    img = nilearn.image.load_img(path_train + 'mwp1train_' + str(i + 1) + '.nii')
+    d = img.get_data()
+    d = d[cut['cut_x'][0]:cut['cut_x'][1],  cut['cut_y'][0]:cut['cut_y'][1],  cut['cut_z'][0]:cut['cut_z'][1]]
+    x.append(np.ravel(d))
+
+for i in range(0, test_size):
+    img = nilearn.image.load_img(path_test + 'mwp1test_' + str(i + 1) + '.nii')
+    d = img.get_data()
+    d = d[cut['cut_x'][0]:cut['cut_x'][1],  cut['cut_y'][0]:cut['cut_y'][1],  cut['cut_z'][0]:cut['cut_z'][1]]
+    t.append(np.ravel(d))
+
+x = np.array(x)
+t = np.array(t)
+
+y = np.genfromtxt('../data/targets.csv', delimiter=',')
+y = y[0:train_size]
+
+# Would like:
+# Create 3x selectKbest, fit for every label
+# Intrinsic just use that
+# Relational classifs transform only the original part
+
+# Every label has its own transformer
+BEST = 8000
+# Intrinsic classifiers
+intrinsics = []
+for lbl in range(0, 3):
+    intrinsics.append(Pipeline([('f_classif', SelectKBest(f_classif, k=BEST)), ('svm', LinearSVC())]))
+
+# Fit every label to its own transformer and add to relational classifiers
+relationals = []
+for lbl in range(0, 3):
+    this_lbl_transformer = SelectKBest(f_classif, k=BEST)
+    this_lbl_transformer.fit(x, y[:, lbl])
+    relationals.append(Relational(this_lbl_transformer, LinearSVC()))
+
+
+clf = MultilabelPredictor()
+print('==== Fitting... ====')
+clf.fit(x, y, intrinsics, relationals)
+print('==== Predicting... ====')
+results = clf.predict(t)
+print(results)
+results = np.ravel(results)
+
+labels = ['gender', 'age', 'health']
+f = open('final_sub.csv', 'w+')
+n = len(results)
+f.write('ID,Sample,Label,Predicted\n')
+i = 0
+while i < n:
+    for ii in range(3):
+        f.write(str(i) + ',' + str(i/3) + ',' + labels[i % 3] + ',' + str(bool(results[i])) + '\n')
+        i += 1
---
+++ multilabel.py
@@ -0,0 +1,134 @@
+import numpy as np
+import itertools
+from sklearn import cross_validation
+
+class MultilabelPredictor():
+
+    def __init__(self):
+        self.intrinsic = []
+        self.relational = []
+        self.order = None
+
+    '''
+        X: data instances (shape n_samples x n_dimensions)
+        y: multilabel data (shape n_samples x n_labels)
+        intrinsic_classifiers: array of classifiers (len: n_labels)
+        relational_classifiers: also n_labels classifiers, but those should also
+        implement their own transform function. The data will first be transformed
+        by that method, then the predict method will be called with the additional
+        features.
+    '''
+    def fit(self, X, y, intrinsic_classifiers, relational_classifiers):
+
+        mean_intrinsic_scores = []
+
+        for label, clas in enumerate(intrinsic_classifiers):
+            # Train intrinsic classifiers: they are normal binary classifiers`
+            # that just look at the data and classify one label.
+            clas.fit(X, y[:, label])
+            self.intrinsic.append(clas)
+
+            cv = cross_validation.cross_val_score(clas, X, y[:, label], cv=5, scoring='accuracy')
+            mean_intrinsic_scores.append(np.mean(cv))
+
+        mean_relational_scores = []
+
+        for label, clas in enumerate(relational_classifiers):
+            # Train relational classifiers: in addition to the data, they look
+            # at the other labels (they still classify one label).
+
+            XT = clas.transform(X)
+            X_relational = np.hstack(itertools.chain((XT,), self.all_but_one(y, label)))
+            clas.fit(X_relational, y[:, label])
+            self.relational.append(clas)
+
+            cv = cross_validation.cross_val_score(clas, X_relational, y[:, label], cv=5, scoring='accuracy')
+            mean_relational_scores.append(np.mean(cv))
+
+
+        improv = np.array(mean_relational_scores) - np.array(mean_intrinsic_scores)
+        self.order = reversed(list(np.argsort(improv)))
+
+    def predict(self, Z):
+        pred = np.zeros(shape=(len(Z), len(self.intrinsic)))
+
+        # How many entries changed in the last iteration
+        change_amount = len(Z) * len(self.intrinsic)
+
+        print('Predicting intrinsically')
+        for label in range(len(self.intrinsic)):
+            pred[:, label] = self.intrinsic[label].predict(Z)
+
+        print('Predicting with relations')
+        ITER = 0
+
+        # Pre-bake the transformed data for every relational classifier
+        all_Z_transformed = []
+        for rel_clf in self.relational:
+            ZT = rel_clf.transform(Z)
+            all_Z_transformed.append(ZT)
+
+
+        while change_amount > 10: # Totally arbitrary
+            print('Iteration ' + str(ITER))
+            ITER += 1
+            change_amount = 0
+
+            for label in self.order:
+                # Obtain data for this classifier
+                thisZ = all_Z_transformed[label]
+                Z_relational = np.hstack(itertools.chain((thisZ,), self.all_but_one(pred, label)))
+                pred_label = self.relational[label].predict(Z_relational)
+
+                # Compute amount of change for this label
+                equal = pred_label == pred[:, label]
+                change_amount += len(equal) - len(equal[equal])
+
+                # Update the predictions
+                pred[:, label] = pred_label
+
+            print('Change: ' + str(change_amount))
+
+        return pred
+
+    '''
+    Returns the opposite of y[:, col] (i.e. all columns except col)
+    as a generator.
+    '''
+    def all_but_one(self, y, col):
+        for i in range(y.shape[1]):
+            if (i == col):
+                continue
+            yield y[:, i].reshape(len(y), 1)
+
+
+# A convenience class for the relational classifiers
+class Relational():
+
+    '''
+    Note: the transformer here should have already been fitted
+    '''
+    def __init__(self, transformer, predictor):
+        self.transformer = transformer
+        self.predictor = predictor
+
+    def transform(self, X):
+        return self.transformer.transform(X)
+
+    '''
+    Fit on the already-transformed data X and the labels y
+    '''
+    def fit(self, X, y):
+        return self.predictor.fit(X, y)
+
+    '''
+    Predict from the already-transformed data Z
+    '''
+    def predict(self, Z):
+        return self.predictor.predict(Z)
+
+    def get_params(self, deep=True):
+         if deep:
+             return self.predictor.get_params(deep) | self.transformer.get_params(deep)
+         else:
+             return { 'transformer': self.transformer, 'predictor': self.predictor }
